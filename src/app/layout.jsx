bad6cad597db258e7c54ee4a48a2e67ea1756a15
Uladzislau Kavalski <vladislav.kovalskij@gmail.com>
2023-01-21 12:29:19 +0100

    added eslint

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,5 @@
-import './globals.css'
+import React from 'react';
+import './globals.css';
 
 export default function RootLayout({ children }) {
   return (
@@ -10,5 +11,5 @@ export default function RootLayout({ children }) {
       <head />
       <body>{children}</body>
     </html>
-  )
+  );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,9 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
+import React from 'react';
+import Image from 'next/image';
+import { Inter } from '@next/font/google';
+import styles from './page.module.css';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
@@ -87,5 +88,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  )
+  );
 }
